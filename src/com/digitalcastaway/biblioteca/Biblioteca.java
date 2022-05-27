45ea49605a3db8ed9457ba366dcd8e89ba31f96c
Lillyana Marrero Caparroso <usr0986@salesianos-lacuesta.net>
2022-05-27 12:49:36 +0100

    initial commit

--- src/com/digitalcastaway/biblioteca/Biblioteca.java
+++ src/com/digitalcastaway/biblioteca/Biblioteca.java
@@ -10,6 +10,7 @@ public class Biblioteca {
 
     public Biblioteca(String name) {
         this.name = name;
+                                                    //esto esta bien
         this.catalogoLibros = new ArrayList<String>(Arrays.stream(Catalogo.libros).toList());
     }
 
